ae971c548742f5270535a83c1054ad69cd38b533
Jhantu Samui <106798967+Jhantu420@users.noreply.github.com>
2024-07-12 12:14:26 +0530

    Delete server directory

--- server/backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connecttoMongo = () => {
-  mongoose.connect(process.env.db_uri).then((data) => {
-    console.log(`DataBase connected Sucessfully  with ${data.connection.host}`);
-  });
-};
-module.exports = connecttoMongo;
